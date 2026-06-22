0952a9d11a1334dcda89a1ef3d37e50088bf249a
Kishan DV <75621509+StrataDragon@users.noreply.github.com>
2026-06-22 23:32:58 +0530

    Update QuickSort.java

--- QuickSort.java
+++ QuickSort.java
@@ -1,52 +1,52 @@
-package hii;
-//6.Implement a Java program to sort a given set of elements using Quick sort method and find the time required to sort the elements
+package timepass;
 import java.util.*;
-public class QuickSort {
-	static int partition(int arr[],int low,int high) {
-		int i=low+1;
-		int j=high;
-		int pivot=arr[low];
-		while(true) {
-			while(i<=high && arr[i]<=pivot) {
-				i++;
-			}
-			while(arr[j]>pivot){
-				j--;
-			}
-			if(i<j) {
-				int temp=arr[i];
-				arr[i]=arr[j];
-				arr[j]=temp;
-			}else break;}
-			int temp=arr[low];
-			arr[low]=arr[j];
-			arr[j]=temp;
-			return j;
-	
-	}
-	static void quicksort(int arr[],int low,int high) {
-		if(low<high){
-			int p=partition(arr,low,high);
-			quicksort(arr,low,p-1);
-			quicksort(arr,p+1,high);
-		}
-	}
-	public static void main(String []args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter n:");
-		int n=sc.nextInt();
-		int arr[]=new int[n];
-		for(int i=0;i<n;i++) {
-			arr[i]=sc.nextInt();
-		}
-		long start=System.nanoTime();
-		quicksort(arr,0,n-1);
-		long end=System.nanoTime();
-		System.out.println("Sorted array:");
-		for(int x=0;x<n;x++) {
-			System.out.print(arr[x]+" ");
-		}
-		System.out.print("Time taken: "+(end-start));
-	}
-	}
+import java.util.Random;
 
+class quicksort {
+
+    static int partition(int arr[], int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for(int j = low; j < high; j++) {
+            if(arr[j] < pivot) {
+                i++;
+                // swap
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        // place pivot in correct position
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    static void quick(int arr[], int low, int high) {
+        if(low < high) {
+            int pi = partition(arr, low, high);
+            quick(arr, low, pi - 1);   // sort left of pivot
+            quick(arr, pi + 1, high);  // sort right of pivot
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Random rand = new Random();
+        int a[] = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = rand.nextInt(100);
+
+        long s = System.nanoTime();
+        quick(a, 0, n-1);
+        long e = System.nanoTime();
+
+        for(int i = 0; i < n; i++)
+            System.out.print(a[i] + " ");
+        System.out.println("\nTime taken:" + (e-s) + "ns");
+    }
+}
